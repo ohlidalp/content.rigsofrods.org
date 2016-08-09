2bbb0b4643fbecd1783b4483186e56c1df43b901
Petr Ohlídal <petr@ohlidal.cz>
2016-08-09 20:27:29 +0200

    Added basic design to mainpage.

--- repo/index.php
+++ repo/index.php
@@ -2,28 +2,28 @@
 
 require_once "global.include.php";
 
-echo "<pre>
-HELLO!
-
-<a href='list-pages.php'>List all threads</a>
-
-";
-
 $query = $db->query("
 	SELECT t.*
 	FROM ".TABLE_PREFIX."ror_repo_threads rt 
         LEFT JOIN ".TABLE_PREFIX."threads t ON rt.tid = t.tid  
 ");
 
-echo "
-PUBLISHED THREADS ({$db->num_rows($query)})
-----------------
-";
+$html = "<h1>All published content  ({$db->num_rows($query)})</h1>
+    <a href='list-pages.php' style='padding-bottom: 10px; display: inline-block;'>List all threads</a>";
 
 while($t = $db->fetch_array($query))
 {
-    echo "[{$t['tid']}] <a href='show-page.php?tid={$t['tid']}'>{$t['subject']}</a>\n";
+    $page_url = "show-page.php?tid={$t['tid']}";
+    
+    $html .='
+        <div style="border: 2px solid lightgray; margin-bottom: 5px;">
+            <a href='.$page_url.'><h2 style="background-color: #EE7700; padding: 0 10px;">'.$t['subject'].'</h2></a>
+            <p style="padding-left: 10px; padding-right: 10px;">Lorem ipsum rigs of rods</p>
+            <p style="padding-left: 10px; padding-right: 10px;">Downloads: TODO</p>
+        </div>';
+    
 }
 
-echo "</pre>";
+$ht_title = "{$t['subject']} - {$repo_config['html_basic_title']}";
+repo_html_template($ht_title, $t['subject'], $html);
 
